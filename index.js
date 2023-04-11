9ce1ec1f68bb399de2bf1d6dc0a93beb3f14b08a
White Bull <blueded88@gmail.com>
2023-04-11 08:52:41 -0400

    Making a few changes

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 let myLibrary = [];
 
-function Book() {
-  // click button to open form
-  const container = document.querySelector('.wrap');
-  const clickBook = document.querySelector('.click-book');
-  const formBox = document.querySelector('.form');
-  clickBook.addEventListener('click', () => {
-    formBox.style.display = formBox.style.display === 'block'
-      ? 'none' : 'block';
-  });
-  container.addEventListener('click', () => {
-    formBox.style.display = 'none';
-  });
-  const arr = [];
-  console.log(typeof (arr.length), ' My length');
+class Book extends Library {
+  Box() {
+    super.Box();
+
+    // click button to open form
+    const container = document.querySelector('.wrap');
+    const clickBook = document.querySelector('.click-book');
+    const formBox = document.querySelector('.form');
+    clickBook.addEventListener('click', () => {
+      formBox.style.display = formBox.style.display === 'block'
+        ? 'none' : 'block';
+    });
+    container.addEventListener('click', () => {
+      formBox.style.display = 'none';
+    });
+  }
 }
-
-Book.prototype = Object.create(Library.prototype);
-
-const newBook = new Book();
-newBook.addBookToLibrary();
+const newBook = new Book('Player Of the Match');
+newBook.Box();
 mapBooks();
 welcomeFunc();
--- js-folder/addToLib.js
+++ js-folder/addToLib.js
@@ -1,59 +1,59 @@
 // Add to Library
-function Library() {}
+class Library {
+  Box() {
+    const welcome = document.querySelector('.welcome-box');
+    let formBox = document.querySelector('.form');
+    formBox.addEventListener('submit', (btn) => {
+      btn.preventDefault();
+      const author = document.querySelector('.authorInput');
+      const titleInput = document.querySelector('.titleInput');
+      const numOfPagesInput = document.querySelector('.numOfPagesInput');
+      const checkBox = document.querySelector('#checkBox');
 
-Library.prototype.addBookToLibrary = () => {
-  const welcome = document.querySelector('.welcome-box');
-  let formBox = document.querySelector('.form');
-  formBox.addEventListener('submit', (btn) => {
-    btn.preventDefault();
-    const author = document.querySelector('.authorInput');
-    const titleInput = document.querySelector('.titleInput');
-    const numOfPagesInput = document.querySelector('.numOfPagesInput');
-    const checkBox = document.querySelector('#checkBox');
-
-    if (formBox.name === 'edit') {
-      if ((author.value.length <= 3)
+      if (formBox.name === 'edit') {
+        if ((author.value.length <= 3)
       || (titleInput.value.length <= 3)
       || (Number(numOfPagesInput.value) <= 5)) {
-        return false;
-      }
-      const libIndex = myLibrary.findIndex((lib) => lib['_id'] === formBox.nonce);
-      const bookObj = {
-        _id: String(formBox.nonce),
-        author: author.value,
-        title: titleInput.value,
-        numOfPages: numOfPagesInput.value,
-        isRead: checkBox.checked,
-      };
-      myLibrary[libIndex] = bookObj;
-      rmInput();
-      formBox.style.display = 'none';
-    } else {
-      if ((author.value.length <= 3)
+          return false;
+        }
+        const libIndex = myLibrary.findIndex((lib) => lib['_id'] === formBox.nonce);
+        const bookObj = {
+          _id: String(formBox.nonce),
+          author: author.value,
+          title: titleInput.value,
+          numOfPages: numOfPagesInput.value,
+          isRead: checkBox.checked,
+        };
+        myLibrary[libIndex] = bookObj;
+        rmInput();
+        formBox.style.display = 'none';
+      } else {
+        if ((author.value.length <= 3)
       || (titleInput.value.length <= 3)
       || (Number(numOfPagesInput.value) <= 5)) {
-        return false;
+          return false;
+        }
+        welcome.style.display = 'none';
+        const bookObj = {
+          _id: String(myLibrary.length),
+          author: author.value,
+          title: titleInput.value,
+          numOfPages: numOfPagesInput.value,
+          isRead: checkBox.checked,
+        };
+        myLibrary.push(bookObj);
+        rmInput();
       }
-      welcome.style.display = 'none';
-      const bookObj = {
-        _id: String(myLibrary.length),
-        author: author.value,
-        title: titleInput.value,
-        numOfPages: numOfPagesInput.value,
-        isRead: checkBox.checked,
-      };
-      myLibrary.push(bookObj);
-      rmInput();
-    }
 
-    mapBooks();
-    function rmInput() {
-      author.value = '';
-      titleInput.value = '';
-      numOfPagesInput.value = '';
-      checkBox.checked = false;
-      formBox.name = '';
-      formBox.nonce = '';
-    }
-  });
-};
+      mapBooks();
+      function rmInput() {
+        author.value = '';
+        titleInput.value = '';
+        numOfPagesInput.value = '';
+        checkBox.checked = false;
+        formBox.name = '';
+        formBox.nonce = '';
+      }
+    });
+  }
+}
